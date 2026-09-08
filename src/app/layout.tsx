882cea93fa9a7a1fba5db16bdc647d7affcc61fe
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2026-09-08 09:33:26 +0900

    스피드 인사이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 
-import "./globals.css";
-import { MSWProvider } from "@/providers/MSWProvider";
+import './globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({
   children,
@@ -10,7 +10,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
